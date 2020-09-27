524946f0e28c161d86dbb451f6411a37a5f30591
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-09-27 10:49:48 -0500

    Wrup up getting started

--- Messenger/Controllers/ConversationViewController.swift
+++ Messenger/Controllers/ConversationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationViewController.swift
 //  Messenger
 //
 //  Created by Baiaman Gapparov on 9/27/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
